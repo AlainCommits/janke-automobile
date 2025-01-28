608c4063f92d67cfa3f6fd959d8d74931af591b9
AlainCommits <alain.lilic1@gmail.com>
2025-01-28 16:09:45 +0100

    resend

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,15 +1,20 @@
 import { EmailTemplate } from '@/components/email';
 import { Resend } from 'resend';
 
+// Instanz von Resend erstellen
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(req: Request) {
   try {
+    // Warten, bis das React-Template korrekt gerendert wurde
+    const emailContent = await EmailTemplate({ firstName: 'John' });
+
+    // Senden der E-Mail
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: ['delivered@resend.dev'],
       subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
+      react: emailContent, // Hier geben wir das gerenderte ReactNode weiter
     });
 
     if (error) {
